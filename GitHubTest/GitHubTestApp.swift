f645864fee6fcc497bd5d67db72c772d0832dd14
Mrs Staves <estaves@st-francis.herts.sch.uk>
2023-09-29 17:03:47 +0100

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by SFCMacMini1 on 29/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
